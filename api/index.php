352223d64cf6ee64bbac3916a0caa877781f2f75
yvess66 <zehandrag@gmail.com>
2026-08-19 15:18:20 +0700

    sda

--- api/index.php
+++ api/index.php
@@ -1,3 +1,24 @@
 <?php
 
+// Paksa Laravel menggunakan folder /tmp untuk cache & logs di Vercel
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+putenv('APP_STORAGE=/tmp/storage');
+
+// Buat struktur folder di dalam /tmp jika belum ada
+$storageDirs = [
+    '/tmp/storage',
+    '/tmp/storage/app',
+    '/tmp/storage/framework',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
